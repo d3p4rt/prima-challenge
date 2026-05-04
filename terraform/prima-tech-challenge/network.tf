18a791681f6199a09c32cd54ff8f84c612fb89fa
d3p4rt <d3p4rt@protonmail.com>
2026-05-04 17:55:11 +0200

    Added SG

--- terraform/prima-tech-challenge/network.tf
+++ terraform/prima-tech-challenge/network.tf
@@ -124,3 +124,58 @@ resource "aws_security_group_rule" "nodes_ingress_https" {
   security_group_id = aws_security_group.eks_nodes.id
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group" "alb" {
+  name        = "${var.project}-alb-sg"
+  description = "Security group for ALB managed by EKS Ingress"
+  vpc_id      = aws_vpc.eks-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "nodes_ingress_app" {
+  description       = "Allow ALB to reach the app"
+  type              = "ingress"
+  from_port         = 5000
+  to_port           = 5000 
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.alb.id
+}
+
+resource "aws_security_group_rule" "nodes_ingress_cluster_everything" {
+  description              = "Allow all traffic from cluster control plane to nodes"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.eks_cluster.id
+}
+resource "aws_security_group_rule" "cluster_ingress_all_from_nodes" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+}

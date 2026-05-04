ebe242ef39617c6b5ddff9eeb994286b7a7a6dc7
d3p4rt <d3p4rt@protonmail.com>
2026-05-04 20:31:23 +0200

    Change endpoint

--- terraform/prima-tech-challenge/eks.tf
+++ terraform/prima-tech-challenge/eks.tf
@@ -33,6 +33,9 @@ resource "aws_eks_cluster" "eks_cluster" {
   vpc_config {
     subnet_ids         = aws_subnet.public[*].id
     security_group_ids = [aws_security_group.eks_cluster.id]
+    endpoint_public_access  = true
+    endpoint_private_access = true
+
   }
 
   access_config {

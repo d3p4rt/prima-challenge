982c02695f6b087feaf1ee6c562513448d5c39b6
d3p4rt <d3p4rt@protonmail.com>
2026-05-04 20:49:36 +0200

    Added new SG

--- terraform/prima-tech-challenge/network.tf
+++ terraform/prima-tech-challenge/network.tf
@@ -186,6 +186,6 @@ resource "aws_security_group_rule" "cluster_ingress_https_public" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"] 
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.eks_cluster.id
 }
